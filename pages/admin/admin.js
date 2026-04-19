c2862411c6a1040740120847b483a186a847964e
ahmed2400640 <ahmed2400640@miuegypt.edu.eg>
2026-04-20 00:41:47 +0200

    Update admin.js

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -280,7 +280,3 @@ window.approveCompany = approveCompany;
 window.rejectCompany = rejectCompany;
 window.approveMentor = approveMentor;
 window.rejectMentor = rejectMentor;
-window.blockUser = blockUser;
-window.unblockUser = unblockUser;
-window.deleteUserAccount = deleteUserAccount;
-window.loadAllUsers = loadAllUsers;
