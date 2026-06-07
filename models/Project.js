2208687cce5d90279ed95dabfa961125a4d932bc
omarmoaz-22 <omar2401619@miuegypt.edu.eg>
2026-06-07 22:38:55 +0300

    Update Project.js

--- models/Project.js
+++ models/Project.js
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema(
+  {
+    mentorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    internshipId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Internship'
+    },
+    company: { type: String, default: '' },
+    category: { type: String, default: '' },
+
+    title: { type: String, required: true },
+    description: { type: String, default: '' },
+    deadline: { type: Date },
+    instructions: { type: String, default: '' },
+    targetYear: { type: String, default: 'any' },
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Project', projectSchema);
