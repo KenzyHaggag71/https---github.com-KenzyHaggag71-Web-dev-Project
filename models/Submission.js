277a2724a5734a85b3b12b2764cfa7247701ac1e
omarmoaz-22 <omar2401619@miuegypt.edu.eg>
2026-06-07 22:39:55 +0300

    Update Submission.js

--- models/Submission.js
+++ models/Submission.js
@@ -1,1 +1,36 @@
 const mongoose = require('mongoose');
+
+const submissionSchema = new mongoose.Schema(
+  {
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Project',
+      required: true
+    },
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+
+    title: { type: String, required: true },
+   link: {
+  type: String,
+  required: true,
+  trim: true
+},
+    description: { type: String, default: '' },
+
+    status: {
+      type: String,
+      enum: ['submitted', 'evaluated'],
+      default: 'submitted'
+    },
+
+    grade: { type: String, default: '' },
+    feedback: { type: String, default: '' }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Submission', submissionSchema);
